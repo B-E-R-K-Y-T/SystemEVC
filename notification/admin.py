59a4c554286cb6e518ad406cf2fe0b6e4217fa17
BERKYT <BerkyT115@yandex.ru>
2024-11-20 18:47:14 +0300

    refactor admin.py

--- notification/admin.py
+++ notification/admin.py
@@ -20,20 +20,25 @@ from .models import (
 )
 
 # Регистрация всех моделей в админке
-admin.site.register(CustomUser)
-admin.site.register(Role)
-admin.site.register(Department)
-admin.site.register(Position)
-admin.site.register(UserStatus)
-admin.site.register(Work)
-admin.site.register(WorkStatus)
-admin.site.register(WorkPriority)
-admin.site.register(Service)
-admin.site.register(Server)
-admin.site.register(ServerType)
-admin.site.register(Location)
-admin.site.register(OperatingSystem)
-admin.site.register(ServiceType)
-admin.site.register(ServiceDependency)
-admin.site.register(WorkLog)
-admin.site.register(Notification)
+models = [
+    Role,
+    Department,
+    Position,
+    UserStatus,
+    Work,
+    WorkStatus,
+    WorkPriority,
+    Service,
+    Server,
+    ServerType,
+    Location,
+    OperatingSystem,
+    ServiceType,
+    ServiceDependency,
+    WorkLog,
+    Notification,
+    CustomUser,
+]
+
+for model in models:
+    admin.site.register(model)
